f489107c5aa3ee7c10603ae5ca376fee91e8edce
Navid Fayezi <navidfayezi.98@gmail.com>
2026-06-18 17:44:49 -0600

    Add a function to sign data

--- src/signature.c
+++ src/signature.c
@@ -14,7 +14,7 @@ EVP_PKEY* load_private_key_from_file(const char* filename) {
         return NULL;
     }
     decoder_ctx = OSSL_DECODER_CTX_new_for_pkey(&pkey, NULL, NULL, NULL, 0, NULL, NULL);
-    if (decoder_ctx == NULL){
+    if (decoder_ctx == NULL) {
         fprintf(stderr, "Error creating decoder context\n");
         BIO_free(bio);
         return NULL;
@@ -30,4 +30,63 @@ EVP_PKEY* load_private_key_from_file(const char* filename) {
     OSSL_DECODER_CTX_free(decoder_ctx);
     BIO_free(bio);
     return pkey;
+}
+
+int sign_RSA_PSS_SHA3_512(const char *message,
+                          size_t message_len, 
+                          EVP_PKEY *private_key,
+                          size_t *signature_len,
+                          unsigned char **signature) { // *signature should be NULL
+    
+    
+    EVP_MD_CTX *mdctx;
+    OSSL_PARAM params[2];
+
+    *signature = NULL;
+
+    mdctx = EVP_MD_CTX_new();
+    if (mdctx == NULL) {
+        fprintf(stderr, "Error creating digest context\n");
+        return 0;
+    }
+    params[0] = OSSL_PARAM_construct_utf8_string(OSSL_SIGNATURE_PARAM_PAD_MODE,
+        OSSL_PKEY_RSA_PAD_MODE_PSS, 0);
+    params[1] = OSSL_PARAM_construct_end();
+
+    if (EVP_DigestSignInit_ex(mdctx, NULL, "SHA3-512", 
+            NULL, NULL, private_key, params)
+            != 1) {
+            
+            fprintf(stderr, "Error Initializing the signing context\n");
+            goto error;
+            }
+
+    if (EVP_DigestSignUpdate(mdctx, message, message_len) != 1){
+        fprintf(stderr, "Error updating the context with hash of message\n");
+        goto error;
+    }
+
+    if (EVP_DigestSignFinal(mdctx, NULL, signature_len) != 1) {
+        fprintf(stderr, "Error obtaining the length of the signature\n");
+        goto error;
+    }
+    *signature = OPENSSL_malloc(*signature_len);
+    if (*signature == NULL) {
+        fprintf(stderr, "Error allocating memory for signature\n");
+        goto error;
+    }
+
+    if(EVP_DigestSignFinal(mdctx, *signature, signature_len) != 1){
+        fprintf(stderr, "Error signing the data\n");
+        goto error;
+    }
+    
+    EVP_MD_CTX_free(mdctx);
+    return 1;
+
+error:
+    ERR_print_errors_fp(stderr);
+    EVP_MD_CTX_free(mdctx);
+    OPENSSL_free(*signature);
+    return 0;
 }
