e1b92668ceb577490cffe9f42abc6f969f7eba26
Navid Fayezi <navidfayezi.98@gmail.com>
2026-06-19 13:26:05 -0600

    Add function signature for the siging function

--- headers/signature.h
+++ headers/signature.h
@@ -2,4 +2,9 @@
 #define SIGNATURE_H
 
 EVP_PKEY* load_private_key_from_file(const char* filename);
+int sign_RSA_PSS_SHA3_512(const char *message,
+                          size_t message_len, 
+                          EVP_PKEY *private_key,
+                          size_t *signature_len,
+                          unsigned char **signature);
 #endif // SIGNATURE_H
