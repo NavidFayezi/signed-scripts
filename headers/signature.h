addeb06c28478a7164e6abc74cc6f0274f68725f
Navid Fayezi <navidfayezi.98@gmail.com>
2026-06-25 14:49:39 -0600

    Add the prototype for the verification function

--- headers/signature.h
+++ headers/signature.h
@@ -1,10 +1,19 @@
 #ifndef SIGNATURE_H
 #define SIGNATURE_H
+#include "common.h"
 
 EVP_PKEY* load_key_from_file(const char* filename);
+
 int sign_RSA_PSS_SHA3_512(const char *message,
                           size_t message_len, 
                           EVP_PKEY *private_key,
                           size_t *signature_len,
                           unsigned char **signature);
+
+int verify_signature_RSA_PSS_sha3_512(const char * message,
+                                      size_t message_len,
+                                      unsigned char * signature, 
+                                      size_t signature_len,
+                                      EVP_PKEY * public_key);
+
 #endif // SIGNATURE_H
